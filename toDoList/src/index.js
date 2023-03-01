c086f6629427b0616f3b3e4bdb6558aa0bb9f5a1
Malcom Charles <61559987+Malcom-Charlie@users.noreply.github.com>
2023-03-01 11:15:24 +0300

    Code review changes

--- toDoList/src/index.js
+++ toDoList/src/index.js
@@ -1,5 +1,13 @@
 /* eslint-disable no-use-before-define */
 import './style.css';
+import { DateTime } from './modules/luxon.js';
+import EditFunctions from './modules/newFunctionality.js';
+
+const dateTimeNow = document.querySelector('.currentTime');
+setInterval(() => {
+  const current = DateTime.now().toLocaleString(DateTime.DATETIME_MED_WITH_WEEKDAY);
+  dateTimeNow.textContent = current;
+}, 1000);
 
 const addForm = document.querySelector('#add-form');
 const todoList = document.querySelector('#todo-list');
@@ -7,86 +15,24 @@ const removeCompletedBtn = document.querySelector('#remove-completed');
 
 let tasks = [];
 
-// Function to add a new task to the array and render it
-const addTask = (taskDescription, completed = false) => {
-  tasks.push({ description: taskDescription, completed });
-  renderTasks();
-};
+const editFunctionality = new EditFunctions(tasks, todoList);
 
-// Function to toggle a task's completed status and render the updated list
-const toggleCompleted = (index) => {
-  tasks[index].completed = !tasks[index].completed;
-  renderTasks();
-  const selectedCheckboxes = todoList.querySelectorAll('input[type="checkbox"]:checked');
-  selectedCheckboxes.forEach((checkbox) => {
-    const taskItem = checkbox.parentNode;
-    const taskIndex = Array.from(todoList.children).indexOf(taskItem);
-    tasks[taskIndex].completed = true;
-    taskItem.classList.add('completed');
-    checkbox.setAttribute('checked', '');
-  });
-  const unselectedCheckboxes = todoList.querySelectorAll('input[type="checkbox"]:not(:checked)');
-  unselectedCheckboxes.forEach((checkbox) => {
-    const taskItem = checkbox.parentNode;
-    const taskIndex = Array.from(todoList.children).indexOf(taskItem);
-    tasks[taskIndex].completed = false;
-    taskItem.classList.remove('completed');
-    checkbox.removeAttribute('checked');
-  });
-};
-
-// Function to render the tasks in the todoList
-const renderTasks = () => {
-  // Clear the todoList
-  todoList.innerHTML = '';
-
-  // Render each task
-  tasks.forEach((task, index) => {
-    const taskItem = document.createElement('li');
-    const hr = document.createElement('HR');
-    const taskCheckbox = document.createElement('input');
-    taskCheckbox.type = 'checkbox';
-    taskCheckbox.checked = task.completed;
-    taskCheckbox.addEventListener('change', () => toggleCompleted(index));
-    const taskDescription = document.createTextNode(task.description);
-    taskItem.appendChild(taskCheckbox);
-    taskItem.appendChild(taskDescription);
-    taskItem.appendChild(hr);
-    todoList.appendChild(taskItem);
-  });
-
-  // Update the indexes of the tasks
-  tasks.forEach((task, index) => {
-    task.index = index;
-  });
-
-  // Save the tasks to local storage
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-};
-
-// Function to remove all completed tasks
-const removeCompletedTasks = () => {
-  tasks = tasks.filter((task) => !task.completed);
-  renderTasks();
-};
-
-// Add event listener to the addForm for submitting new tasks
 addForm.addEventListener('submit', (event) => {
   event.preventDefault();
   const newTaskInput = document.querySelector('#new-task');
   const newTaskDescription = newTaskInput.value.trim();
   if (newTaskDescription !== '') {
-    addTask(newTaskDescription);
+    editFunctionality.addTask(newTaskDescription);
     newTaskInput.value = '';
   }
 });
 
-// Add event listener to the removeCompletedBtn to remove all completed tasks
-removeCompletedBtn.addEventListener('click', removeCompletedTasks);
+removeCompletedBtn.addEventListener('click', () => {
+  editFunctionality.removeCompletedTasks();
+});
 
-// Load saved tasks from local storage
 const savedTasks = JSON.parse(localStorage.getItem('tasks'));
 if (savedTasks) {
   tasks = savedTasks;
-  renderTasks();
+  editFunctionality.renderTasks();
 }
